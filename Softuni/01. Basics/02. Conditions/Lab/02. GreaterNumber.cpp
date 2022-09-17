a0704e852d15074db8f31c45cdf278892ed65a03
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-09-17 20:01:30 +0300

    Adding Conditions folder and Lab exercises to it

---
+++ Softuni/01. Basics/02. Conditions/Lab/02. GreaterNumber.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b;
+    cin >> a >> b; 
+    if (a > b) {
+        cout << a << endl;
+
+    }
+    else {
+        cout << b << endl;
+    }
+}
+
---
+++ Softuni/01. Basics/02. Conditions/Lab/07. AreaOfFigures.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string figure;
+	cin >> figure;
+
+	cout.setf(ios::fixed);
+	cout.precision(3);
+
+	if (figure == "square") {
+		double a;
+		cin >> a;
+		cout << a*a << endl;
+	}
+	else if (figure == "rectangle") {
+		double a, b;
+		cin >> a >> b;
+		cout << a * b << endl;
+	}
+	else if (figure == "circle") {
+		double r, pi;
+		cin >> r;
+		pi = 3.14159265359;
+		cout << pi * r * r << endl;
+	}
+	else {
+		double a, ha;
+		cin >> a >> ha;
+		cout << a * ha / 2 << endl;
+	}
+	return 0;
+}
+
+ 
