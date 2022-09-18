da78d62421c1a77c9ff6017c534c81f7fff729f0
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-09-18 14:50:32 +0300

    Correcting the divisions

--- C++ through game development/02.  Simple Calculator/02.  Simple Calculator/02.  Simple Calculator.cpp
+++ C++ through game development/02.  Simple Calculator/02.  Simple Calculator/02.  Simple Calculator.cpp
@@ -7,9 +7,10 @@ int main()
     cout << "12 + 3 = " << 12 + 3 << endl;
     cout << "12 - 2 = " << 12 - 2 << endl;
     cout << "12 / 5 = " << 12 / 5 << endl;  // int - rouding the result
-    cout << "12.0 / 5.0 = " << 12.0 / 7.0 << endl;
+    cout << "12.0 / 5.0 = " << 12.0 / 5.0 << endl;
+    cout << "12.0 / 5 = " << 12.0 / 5 << endl;
     cout << "12 % 5 = " << 12 % 5 << endl; // returns the remainder of INT-division
- 
+    
     return 0;
 }
 
