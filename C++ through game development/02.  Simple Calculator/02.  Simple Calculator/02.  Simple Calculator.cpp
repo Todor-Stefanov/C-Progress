79eafa079073f1ec1ceca9a05f238bbaf3890596
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-09-18 11:34:27 +0300

    Adding some simple calculation

---
+++ C++ through game development/02.  Simple Calculator/02.  Simple Calculator/02.  Simple Calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "12 + 3 = " << 12 + 3 << endl;
+    cout << "12 - 2 = " << 12 - 2 << endl;
+    cout << "12 / 5 = " << 12 / 5 << endl;  // int - rouding the result
+    cout << "12.0 / 5.0 = " << 12.0 / 7.0 << endl;
+    cout << "12 % 5 = " << 12 % 5 << endl; // returns the remainder of INT-division
+ 
+    return 0;
+}
+
