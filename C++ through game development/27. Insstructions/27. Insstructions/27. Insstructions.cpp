29cc7377ebe2b14ce97b96122fe75ec0292abf82
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-10-17 18:59:52 +0300

    Getting to know how functions work

---
+++ C++ through game development/27. Insstructions/27. Insstructions/27. Insstructions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// function prototype (declaration)
+void instructions();
+
+int main()
+{
+	instructions();
+	return 0; 
+}
+
+
+// function definition
+void instructions() {
+	std::cout << "Welcome to the most fun you've ever had with text!" << std::endl;
+	std::cout << "\nHere is how to play the game..." << std::endl;
+}
